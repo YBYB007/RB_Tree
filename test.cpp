d4efad9ba40fd9798923408cd64a1efc2f0dd00c
ybyb <ujn_yb@163.com>
2024-07-06 14:01:59 +0800

    test-

--- test.cpp
+++ test.cpp
@@ -42,3 +42,84 @@ void red_test(){
     T1.print_tree_pre(); 
     std::cout<<"----------------------------------------------------------------------"<<std::endl;
 }
+/*
+// 测试案例
+empty
+|_Node(17, Color: BLACK)root
+----------------------------------------------------------------------
+|_Node(17, Color: BLACK)root
+  |_Node(18, Color: RED)right
+----------------------------------------------------------------------
+|_Node(18, Color: BLACK)root
+  |_Node(17, Color: RED)left
+  |_Node(23, Color: RED)right
+----------------------------------------------------------------------
+|_Node(18, Color: BLACK)root
+  |_Node(17, Color: BLACK)left
+  |_Node(23, Color: BLACK)right
+    |_Node(34, Color: RED)right
+----------------------------------------------------------------------
+|_Node(18, Color: BLACK)root
+  |_Node(17, Color: BLACK)left
+  |_Node(27, Color: BLACK)right
+    |_Node(23, Color: RED)left
+    |_Node(34, Color: RED)right
+----------------------------------------------------------------------
+|_Node(18, Color: BLACK)root
+  |_Node(17, Color: BLACK)left
+    |_Node(15, Color: RED)left
+  |_Node(27, Color: BLACK)right
+    |_Node(23, Color: RED)left
+    |_Node(34, Color: RED)right
+----------------------------------------------------------------------
+|_Node(18, Color: BLACK)root
+  |_Node(15, Color: BLACK)left
+    |_Node(9, Color: RED)left
+    |_Node(17, Color: RED)right
+  |_Node(27, Color: BLACK)right
+    |_Node(23, Color: RED)left
+    |_Node(34, Color: RED)right
+----------------------------------------------------------------------
+|_Node(18, Color: BLACK)root
+  |_Node(15, Color: RED)left
+    |_Node(9, Color: BLACK)left
+      |_Node(6, Color: RED)left
+    |_Node(17, Color: BLACK)right
+  |_Node(27, Color: BLACK)right
+    |_Node(23, Color: RED)left
+    |_Node(34, Color: RED)right
+----------------------------------------------------------------------
+|_Node(18, Color: BLACK)root
+  |_Node(15, Color: RED)left
+    |_Node(8, Color: BLACK)left
+      |_Node(6, Color: RED)left
+      |_Node(9, Color: RED)right
+    |_Node(17, Color: BLACK)right
+  |_Node(27, Color: BLACK)right
+    |_Node(23, Color: RED)left
+    |_Node(34, Color: RED)right
+----------------------------------------------------------------------
+|_Node(15, Color: BLACK)root
+  |_Node(8, Color: RED)left
+    |_Node(6, Color: BLACK)left
+      |_Node(5, Color: RED)left
+    |_Node(9, Color: BLACK)right
+  |_Node(18, Color: RED)right
+    |_Node(17, Color: BLACK)left
+    |_Node(27, Color: BLACK)right
+      |_Node(23, Color: RED)left
+      |_Node(34, Color: RED)right
+----------------------------------------------------------------------
+|_Node(15, Color: BLACK)root
+  |_Node(8, Color: BLACK)left
+    |_Node(6, Color: BLACK)left
+      |_Node(5, Color: RED)left
+    |_Node(9, Color: BLACK)right
+  |_Node(18, Color: BLACK)right
+    |_Node(17, Color: BLACK)left
+    |_Node(27, Color: RED)right
+      |_Node(23, Color: BLACK)left
+        |_Node(25, Color: RED)right
+      |_Node(34, Color: BLACK)right
+----------------------------------------------------------------------
+*/
